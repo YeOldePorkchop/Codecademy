88ea8ca163bea76758fdec77096b1b85285c2d69
YeOldePorkchop-l <michael.delomba@gmail.com>
2020-08-25 16:05:55 -0700

    Added some loops for the loops test

--- Loops/javascript.js
+++ Loops/javascript.js
@@ -1,8 +1,12 @@
 let amountNeeded = 5;
 let amountCompleted = 0;
 
+for (let i = 0; i < 5; i++) {
+  console.log("For Loop: " + i);
+}
+
 do {
-  console.log("Derp: " + amountCompleted);
+  console.log("Do While Loop: " + amountCompleted);
   amountCompleted++;
   // Test
 } while (amountCompleted < amountNeeded);

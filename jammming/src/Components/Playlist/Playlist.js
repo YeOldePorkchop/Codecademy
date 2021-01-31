b5e43f7753688543bc54cd85115cacb0834bd9c6
Michael DeLomba <michael.delomba@gmail.com>
2021-01-30 17:41:52 -0800

    Added ability to change the playlist name.

--- jammming/src/Components/Playlist/Playlist.js
+++ jammming/src/Components/Playlist/Playlist.js
@@ -4,10 +4,27 @@ import './Playlist.css';
 import { TrackList } from '../TrackList/TrackList';
 
 export class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 'New Playlist'
+    }
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(e) {
+    let name = e.target.value;
+    this.props.onNameChange(name);
+    this.setState({value: name});
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div className="Playlist">
-        <input value="New Playlist"/>
+        <input value={this.state.value} onChange={this.handleNameChange} />
         <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
